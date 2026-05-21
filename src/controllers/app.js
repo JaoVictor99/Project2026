f459811ade14dcb816eb27fc3b6937927758711c
João Victor de Souza Silva <jooao.victor.souza99@gmail.com>
2026-05-20 23:23:17 -0300

    Commitando Middlers 20.05

--- src/controllers/app.js
+++ src/controllers/app.js
@@ -1,22 +1,100 @@
-const  http = require('http');
+
+// Imortando o pacote Express
+const express = require("express")
+//Inicializando o Express
+const app = express();
+const port = 3000;
 
 
-const server = http.createServer((req,res) => {
-    res.setHeader('Content-Type', 'texto/html; charset=utf-8');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-    if(req.url === '/'){
-        res.end('<html><head><title><body><h1>Seja bem Vindo ao Sistema</h1></body></title></head></html>')
-    }else if(req.url === '/cliente'){
-        res.end('<h2>Cadastro de Paciente</h2>')
+//Rotas da aplicação
+
+app.get('/', (req,res) => {
+    console.log("Requisição tipo GET realizada na rota /");
+    res.sed("<h1>Bem Vindo ao Sistem XPTO</h1>");
+});
+
+
+app.get('/', (req,res) => {
+    console.log("Requisição do tipo get na rota /CadastroCliente");
+    res.send("<h1>Cadastro Cliente</h1>")
+})
+
+
+app.get('/', (req,res) => {
+    const termoDeBusca = req.query.termo;
+    if(termoDeBusca){
+        console.log("Parametro via query: " + termoDeBusca)
+        res.send(`<h1> Você pesquisou por ${termoDeBusca}`);
+        
     }else{
-        res.statusCode = 404;
-        res.end('<h1>Página não econtrada</h1>');
+        res.send(`<h1> Você não passou o parametro de busca</h1>`);
+    }
+});
+
+//Modelo errado de passar queryParam
+app.get(`/login`, (req,res) => {
+    const pLogin = req.query.login;
+    const pSenha = req.query.senha;
+
+    if(pLogin === 'Alex' && pSenha === '1234'){
+        res.send("<h1> Bem Vindo Alex</h1>")
+    }else{
+        res.send("<h1> Bem Vindo Alex</h1>") 
+    }
+
+    res.send("<h1>Usuario ou senha incorretos</h1>");
+});
+
+
+// NOVO METODO HTTP
+app.post('/recebeform', (req,res) =>{
+    const dados = req.body;
+    console.log("Dados vindo do formulario: ", dados)
+    res.send("Dados enviados!");
+});
+
+
+app.post('/login', (req,res) => {
+    const {login,senha} = req.body
+    console.log(`Login: ${login}, Senha ${Senha}`);
+    if(login ==='alex' && senha === '1234'){
+        res.status(200).send("<h1> Bem vindo ao sistema</h1>")
+    }else{
+        res.status(401).sed("<h1> Login ou senha Incorretos </h1>");
     }
 });
 
 
-const port = 3000;
+app.post('/cadastroProduto', (req,res) => {
+    const {codigo, nome, preco} = req.body;
 
-server.listen(PORT, 'localhost', () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    if(codigo && nome && preco){
+        res.status(202).send(`<h1>Produto Inserido!</h1> <p> ${codigo}, ${nome}, ${preco}</p>`)
+    }else{
+        res.status(400).send("<h1> Voce não informou alguns dos 3 parametros</h1>");
+    }
+
+})
+
+
+    app.put("/editaonibus", (req,res) => {
+        const {numro,linha,lugares} = req.body
+        if(numero && linha && lugares){
+            res.status(200).send(`<h1> Busão Alterado</h1> <p> ${numero}, ${linha}, ${lugares}</p>`)
+        }else{
+            res.status(400).send("<h1>Faltou algum parametro</h1>");
+        }
+    })
+
+
+
+
+
+// Startado o Servidor ma porta 3000
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 })
