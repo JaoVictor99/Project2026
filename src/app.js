d38e19c9f322792a7834088f114bf6d2b36cf304
João Victor de Souza Silva <jooao.victor.souza99@gmail.com>
2026-05-28 21:02:36 -0300

    ConectarBancoDados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const db = require("./db/conn");
+
+db.connect((err) => {
+
+    if(err){
+        console.log("Erro ao conectar no banco");
+        console.log(err);
+    } else {
+        console.log("PostgreSQL conectado!");
+    }
+});
+
 app.use(express.urlencoded({ extended:true }));
 
 app.set("view engine", "ejs");
