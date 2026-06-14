8edeffb8dcc0569561ae9e466f7910ddae163b40
João Victor de Souza Silva <jooao.victor.souza99@gmail.com>
2026-06-14 19:42:28 -0300

    Listar Definitivo

--- src/public/frontDao/home.js
+++ src/public/frontDao/home.js
@@ -1,8 +1,14 @@
-async function carregarAlunos() {
-  const response = await fetch('http://localhost:3000/alunos');
-  const { sucesso, dados } = await response.json();
 
-  if (sucesso) {
-    exibirAlunos(dados);
-  }
-};
+document.getElementById("pesquisa").addEventListener("input", function () {
+    const termo = this.value.toLowerCase();
+    const alunos = document.querySelectorAll(".linha-aluno");
+
+    alunos.forEach(aluno => {
+        const nome = aluno.querySelector(".aluno span").textContent.toLowerCase();
+        if (nome.includes(termo)) {
+            aluno.style.display = "flex";
+        } else {
+            aluno.style.display = "none";
+        }
+    });
+});
