6b5c06fadec627fb722731e822ffafe11bb3dff6
João Victor de Souza Silva <jooao.victor.souza99@gmail.com>
2026-06-15 23:41:22 -0300

    Ultim Commit da Noite

--- src/public/frontDao/cadastroLivro.js
+++ src/public/frontDao/cadastroLivro.js
@@ -2,9 +2,40 @@ async function capturarLivro(){
 
     try{
 
-        const 
+
+
+        const titulo = document.getElementById("titulo").value;
+        const autor = document.getElementById("autor").value;
+        const paginas = document.getElementById("paginas").value;
+        const nacionalidade = document.getElementById("nacionalidade").value;
+
+
+        console.log("Dados capturados:", { titulo, autor, paginas, nacionalidade });
+
+        const resposta = await fetch("http://localhost:3000/cadastroLivro",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                titulo: titulo,
+                autor: autor,
+                paginas: paginas,
+                nacionalidade: nacionalidade,
+            })
+
+            
+        });
+
+        const dados = await resposta.json();
+
+        
 
     }catch(error){
-        
+        console.error(error);
+        return alert("Erro interno do servidor");
+    }finally{
+
+        window.location.href = "/home";
     }
 };
